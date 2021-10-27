fd073ffae9db07dbc72da81ef2ec2e35969c5d0e
frankiefazlollahi <87551126+frankiefazlollahi@users.noreply.github.com>
2021-10-26 21:17:43 -0700

    Delete Data.py

--- ClusterAnalysis/Data.py
+++
@@ -1,8 +0,0 @@
-class Data:
-    def __init__(self, sepalLength, sepalWidth, petalLength, petalWidth, label, id):
-        self.sepalLength = sepalLength
-        self.sepalWidth = sepalWidth
-        self.petalLength = petalLength
-        self.petalWidth = petalWidth
-        self.label = label
-        self.id = id
